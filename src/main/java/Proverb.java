84039ed3af482fed8bc95cca5b9fa2dfae776bb0
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-05-25 08:42:17 -0300

    Exercise solution

--- src/main/java/Proverb.java
+++ src/main/java/Proverb.java
@@ -1,11 +1,20 @@
 class Proverb {
 
+    private final String[] words;
+
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.words = words;
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (words.length == 0) return "";
+
+        StringBuilder proverb = new StringBuilder();
+        for (int i = 1; i < words.length; i++) {
+            proverb.append("For want of a %s the %s was lost.\n".formatted(words[i - 1], words[i]));
+        }
+        proverb.append("And all for the want of a %s.".formatted(words[0]));
+        return proverb.toString();
     }
 
 }
